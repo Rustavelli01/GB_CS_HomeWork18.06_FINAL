b95aac67a7ecc9762cc101d91c5d72e2b36abbc7
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-18 22:41:23 +0600

    Задача 6:Done

---
+++ GB_CS_HomeWork#6/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка)
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.Clear();
+
+Console.Write("Я кибер мозг, дайте мне число: ");
+string userInput = Console.ReadLine() ??"";
+int userNumber = int.Parse(userInput);
+
+int number = userNumber % 2;
+
+if(number == 0)
+{
+    Console.WriteLine($"Да. Это число четное! {userNumber} :-) ");
+}
+else
+{
+    Console.WriteLine($"Нет. Это число не четное! {userNumber} :-( ");
+}
