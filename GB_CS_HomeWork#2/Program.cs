f2d329fc82161ecee9c5ad44e1ad9a30657f2758
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-18 22:54:21 +0600

    Завершаем ДЗ/

--- GB_CS_HomeWork#2/Program.cs
+++ GB_CS_HomeWork#2/Program.cs
@@ -1,70 +1,23 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход три числа и
-// выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44, 5, 78 -> 78
-// 22, 3, 9 -> 22
-
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
 
 Console.Clear();
-
-Console.WriteLine("Давайте поиграем с числами и найдем максимальное... ");
-Console.Write("Введите Ваше первое целое число: ");
+Console.Write("Ваше первое число, Хозяин!: ");
 string userImput1 = Console.ReadLine() ?? "";
 int a = int.Parse(userImput1);
 
-Console.Write("Введите Ваше второе целое число: ");
+Console.Write("Ваше второе число, Хозяин!: ");
 string userImput2 = Console.ReadLine() ?? "";
 int b = int.Parse(userImput2);
 
-Console.Write("Введите Ваше второе целое число: ");
-string userImput3 = Console.ReadLine() ?? "";
-int c = int.Parse(userImput3);
-
-int max=0;
-
-/*if(a>max) max = a;
-if(b>max) max = b;
-if(c>max) max = c;*/
-
-/*if (a > b)
+if( a > b )
 {
-    Console.WriteLine($"Самое большое число: {a}");
+    Console.WriteLine("Ваше первое число " + a + " больше чем второе " + b + " ,Хозяин! ");
 }
-else if (a > c)
+else
 {
-    Console.WriteLine($"Самое большое число: {a}");
+    Console.WriteLine("Ваше второе число " + b + " больше чем первое " + a + " ,Хозяин! ");
 }
-
-else if (a < b)
-{
-    Console.WriteLine($"Самое большое число: {b}");
-}
-
-else if (b > c)
-{
-    Console.WriteLine($"Самое большое число: {b}");
-}
-
-else if (c > a)
-{
-    Console.WriteLine($"Самое большое число: {c}");
-}
-
-else if (c > b)
-{
-    Console.WriteLine($"Самое большое число: {c}");
-}*/
-if(a > max)
-{
-    max = a;
-}
-if(b > max)
-{
-    max = b;
-}
-if(c > max)
-{
-    max = c;
-}
-
-Console.WriteLine("Самое большое число: " + max);
---
+++ GB_CS_HomeWork#4/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+
+Console.Clear();
+
+Console.WriteLine("Давайте поиграем с числами и найдем максимальное... ");
+Console.Write("Введите Ваше первое целое число: ");
+string userImput1 = Console.ReadLine() ?? "";
+int a = int.Parse(userImput1);
+
+Console.Write("Введите Ваше второе целое число: ");
+string userImput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userImput2);
+
+Console.Write("Введите Ваше второе целое число: ");
+string userImput3 = Console.ReadLine() ?? "";
+int c = int.Parse(userImput3);
+
+int max=0;
+
+/*if(a>max) max = a;
+if(b>max) max = b;
+if(c>max) max = c;*/
+
+/*if (a > b)
+{
+    Console.WriteLine($"Самое большое число: {a}");
+}
+else if (a > c)
+{
+    Console.WriteLine($"Самое большое число: {a}");
+}
+
+else if (a < b)
+{
+    Console.WriteLine($"Самое большое число: {b}");
+}
+
+else if (b > c)
+{
+    Console.WriteLine($"Самое большое число: {b}");
+}
+
+else if (c > a)
+{
+    Console.WriteLine($"Самое большое число: {c}");
+}
+
+else if (c > b)
+{
+    Console.WriteLine($"Самое большое число: {c}");
+}*/
+if(a > max)
+{
+    max = a;
+}
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("Самое большое число: " + max);
