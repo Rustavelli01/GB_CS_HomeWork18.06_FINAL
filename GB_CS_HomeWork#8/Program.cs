c3244e8aa6c02887660c4755a4cc280e95d73313
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-18 22:46:23 +0600

    Задача 4:Done

---
+++ GB_CS_HomeWork#8/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Clear();
+
+Console.Write("Какое число хотите разложить?: ");
+string userInput = Console.ReadLine() ??"";
+int Number = int.Parse(userInput);
+
+int userNumber = 2;
+
+if(Number > 1)
+{
+    while(userNumber <= Number)
+    {
+        Console.Write(userNumber + ", ");
+        userNumber = userNumber + 2;
+    }
+}
