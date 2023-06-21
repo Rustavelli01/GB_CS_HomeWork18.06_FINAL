c06f9dc5ffedfce758f7433e9bd3d50257d1a2eb
Rustavelli <135349279+Rustavelli01@users.noreply.github.com>
2023-06-21 22:02:55 +0600

    GB_CS_HomeWork#13.Done

--- GB_CS_HomeWork#13/Program.cs
+++ GB_CS_HomeWork#13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+
+Console.Clear();
+Console.WriteLine("Доброго времени суток! Задача 13 ");
+Console.Write("ВВедите 3-х значное число: ");
+string userImput = Console.ReadLine() ?? "";
+int Num = int.Parse(userImput);
+if (userImput.Length > 2)
+{
+  Console.WriteLine("третья цифра имеет символ -> " + userImput[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет!");
+}
+Console.WriteLine("У Вас осталось 2 попытки! ");
+Console.WriteLine();
